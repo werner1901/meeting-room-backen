2bd9f5456e1b8c131ca1dec462a50ad3438bdb8c
田悦廷 <werner1901@163.com>
2025-02-22 15:42:25 +0800

    init:ch111，但邮件登录未做

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,27 @@
+import { Global, Module } from '@nestjs/common';
+import { RedisService } from './redis.service';
+import { createClient } from 'redis';
+
+// 声明为全局模块就不用四处导入了
+@Global()
+@Module({
+  providers: [
+    RedisService,
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = createClient({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+          database: 1,
+        });
+        await client.connect();
+        return client;
+      },
+    },
+  ],
+  exports: [RedisService],
+})
+export class RedisModule {}
