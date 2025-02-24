cb0945c2ef6a23a422fc5136f6e76f5c9ff5f682
田悦廷 <werner1901@163.com>
2025-02-24 17:15:18 +0800

    ch118

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 // import { UnloginFilter } from './unlogin.filter';
 import { CustomExceptionFilter } from './custom-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FormatResponseInterceptor());
   // app.useGlobalFilters(new UnloginFilter());
   app.useGlobalFilters(new CustomExceptionFilter());
+  app.useStaticAssets('uploads', {
+    prefix: '/uploads',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,7 +6,8 @@ import {
   Query,
   Inject,
   UnauthorizedException,
-  ParseIntPipe,
+  UseInterceptors,
+  UploadedFile,
   BadRequestException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
@@ -21,6 +22,9 @@ import { UserDetailVo } from './user-info.vo';
 import { UpdateUserPasswordDto } from './vo/update-user-password.dto';
 import { UpdateUserDto } from './dto/udpate-user.dto';
 import { generateParseIntPipe } from 'src/utils/utils';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path';
+import { storage } from 'src/my-file-storage';
 
 @Controller('user')
 export class UserController {
@@ -292,5 +296,28 @@ export class UserController {
       pageNo,
       pageSize,
     );
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 3,
+      },
+      fileFilter(req, file, callback) {
+        const extname = path.extname(file.originalname);
+        if (['.png', '.jpg', '.gif'].includes(extname)) {
+          callback(null, true);
+        } else {
+          callback(new BadRequestException('只能上传图片'), false);
+        }
+      },
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+    return file.path;
   }
 }
