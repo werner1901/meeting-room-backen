3f37157d25d1452a7d1ecf4e352c26ce0545aa7a
田悦廷 <werner1901@163.com>
2025-02-24 15:02:11 +0800

    ch112 已完成

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 <https://www.typeorm.net/custom-repository>
 
 2. 发送邮件验证码已加上。注意用env配置信息时，需要修改nest-cli.json，以及configModule模块需要全局引入后才能用
+
+3. 需要再看一下vo是什么:
+dto 是 data transfer object，用于参数的接收。
+vo 是 view object，用于返回给视图的数据的封装。
+而 entity 是和数据库表对应的实体类。
+
+4. @Query是怎么用的
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, SetMetadata } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequireLogin, RequirePermission, UserInfo } from './custom.decorator';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('aaa')
+  // @SetMetadata('require-login', true)
+  // @SetMetadata('require-permission', ['ccc'])
+  @RequireLogin()
+  @RequirePermission(['ccc'])
+  aaaa(@UserInfo('username') username: string, @UserInfo() userInfo) {
+    console.log(username);
+    console.log(userInfo);
+    return 'aaa';
+  }
+
+  @Get('bbb')
+  bbb() {
+    return 'bbb';
   }
 }
